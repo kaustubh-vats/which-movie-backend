c35b2cb61efdd3ed8639f386bd8205e113f58342
Kaustubh Vats <kaustubhvats08@gmail.com>
2023-03-13 21:30:24 +0530

    Initial commit

--- api/app.py
+++ api/app.py
@@ -170,5 +170,4 @@ def not_found(e):
 def internal_error(e):
     return {'status': 'Internal Server Error','message': 'Something went wrong'}, 500
 if __name__ == '__main__':
-
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
